7e4d4ae0f1b94725b4cd88b08ccce946577d3b61
e.chebotar <e.chebotar@unic-lab.by>
2023-05-19 22:15:12 +0300

    done port configuration and setup output state

--- gpio.c
+++ gpio.c
@@ -5,14 +5,76 @@
 //=============================================================================
 
 /**
-*
+* Enable clocking for current port
+* - Gpio port
 */
-uint8_t gpio_configure_alternative();
+void gpio_setup_clocks(GPIO_TypeDef* port);
 
 //=============================================================================
 
 
-uint8_t gpio_configure()
+uint8_t gpio_configure(GPIO_Config_s* config)
+{
+	//SetUp clock
+	gpio_setup_clocks(config->port);
+
+	// Configure MODER
+	uint8_t pin_position = config->pin * 2;
+
+	if (pin_position > 30U) return 1;
+
+	config->port->MODER &= ~(3 << pin_position);
+	config->port->MODER |= (config->mode << pin_position);
+
+	// Configure OTYPER
+	config->port->OTYPER |= (config->output << config->pin);
+
+	// Configure OSPEEDR
+	config->port->OSPEEDR &= ~(3 << pin_position);
+	config->port->OSPEEDR |= (config->speed << pin_position);
+
+	// Configure PUPDR
+	config->port->PUPDR &= ~(3 << pin_position);
+	config->port->PUPDR |= (config->pupd << pin_position);
+
+	return 0;
+}
+
+uint8_t gpio_deinit(GPIO_Config_s* config)
 {
 	return 1;
+}
+
+uint8_t gpio_get_state(GPIO_TypeDef* port, GPIO_Pin_e pin)
+{
+	return 1;
+}
+
+void gpio_set_output_state(GPIO_TypeDef* port,
+	GPIO_Pin_e pin,
+	GPIO_State_e state)
+{
+	if (state == GPIO_State_NotConfigure)
+		return;
+	else if (state == GPIO_State_SET)
+		port->BSRR |= (1 << pin);
+	else if (state == GPIO_State_RESET)
+		port->BSRR |= (1 << (pin << 16));
+
+}
+
+void gpio_setup_clocks(GPIO_TypeDef* port)
+{
+	if (port == GPIOA)
+		RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+	else if (port == GPIOB)
+		RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
+	else if (port == GPIOC)
+		RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+	else if (port == GPIOD)
+		RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;
+	else if (port == GPIOE)
+		RCC->AHB1ENR |= RCC_AHB1ENR_GPIOEEN;
+	else
+		return;
 }
--- gpio.h
+++ gpio.h
@@ -2,16 +2,18 @@
 #define _GPIO_H_
 
 #include <stdint.h>
+#include "stm32f407xx.h"
 
 //=============================================================================
 
 typedef enum {
-	GPIO_State_ON,
-	GPIO_State_OFF,
-} GPIO_State;
+	GPIO_State_NotConfigure = -1,
+	GPIO_State_SET = 0,
+	GPIO_State_RESET,
+} GPIO_State_e;
 
 typedef enum {
-	PIN_0,
+	PIN_0 = 0,
 	PIN_1,
 	PIN_2,
 	PIN_3,
@@ -27,23 +29,85 @@ typedef enum {
 	PIN_13,
 	PIN_14,
 	PIN_15,
-} GPIO_Pin;
+} GPIO_Pin_e;
+
+typedef enum {
+	GPIO_Mode_Input = 0,
+	GPIO_Mode_Output,
+	GPIO_Mode_AltFunc,
+	GPIO_Mode_Analog,
+}GPIO_Mode_e;
+
+typedef enum {
+	GPIO_OutputType_PushPull = 0,
+	GPIO_OutputType_OpenDrain,
+}GPIO_OutputType_e;
+
+typedef enum {
+	GPIO_Speed_Low = 0,
+	GPIO_Speed_Medium,
+	GPIO_Speed_High,
+	GPIO_Speed_VeryHigh,
+} GPIO_Speed_e;
+
+typedef enum {
+	GPIO_PUPD_PullUp,
+	GPIO_PUPD_PullDown
+} GPIO_PUPD_e;
+
+typedef struct {
+	GPIO_TypeDef* port;
+	GPIO_Pin_e pin;
+	GPIO_Mode_e mode;
+	GPIO_Speed_e speed;
+	GPIO_OutputType_e output;
+	GPIO_PUPD_e pupd;
+} GPIO_Config_s;
 
 //=============================================================================
 
 /**
-*
+* Configure Pin as Mode
+* - config - Configuration of a specific MCU pin
+* + result
 */
-uint8_t gpio_configure();
+uint8_t gpio_configure(GPIO_Config_s* config);
 
 /**
-*
+* Deinit configuration
+* - config - Configuration
+* + result
 */
-uint8_t gpio_deinit();
+uint8_t gpio_deinit(GPIO_Config_s* config);
 
 /**
-*
+* Get state of pin
+* - port - GPIO port
+* - pin - specific pin if MCU
+* + Pin state (or "Not Configure")
 */
-uint8_t gpio_get_state();
+uint8_t gpio_get_state(GPIO_TypeDef* port, GPIO_Pin_e pin);
+
+/**
+* Set output state
+* - Gpio port
+* - pin
+*/
+void gpio_set_output_state(GPIO_TypeDef* port,
+						   GPIO_Pin_e pin,
+						   GPIO_State_e state);
+
+//=============================================================================
+
+#define GPIO_DEFAULT_OUTPUT(_port, _pin) {	\
+										.port = _port,	\
+										.pin = _pin, 	\
+										.mode = GPIO_Mode_Output, \
+										.speed = GPIO_Speed_High,\
+										.output = GPIO_OutputType_PushPull,\
+										.pupd = GPIO_PUPD_PullUp, \
+										}
+#define GPIO_TOGGLE(port, pin)
+
 
 #endif
